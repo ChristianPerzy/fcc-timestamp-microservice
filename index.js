8da8f5f193d351204b26ad987487a4665a24a48b
ChristianPerzy <christian.perzy@gmail.com>
2022-06-03 01:20:54 +0200

    added endpoint for empty date parameter

--- index.js
+++ index.js
@@ -21,11 +21,12 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+function dateObj(date) {
+  return {
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  };
+}
 
 app.get("/api/:dt", (req, res) => {
   let dt = req.params.dt;
@@ -37,11 +38,12 @@ app.get("/api/:dt", (req, res) => {
       error: date.toString()
     });
   } else {
-    res.json({
-      unix: date.getTime(),
-      utc: date.toUTCString()
-    });
+    res.json(dateObj(date));
   }
+});
+
+app.get("/api/", function (req, res) {
+  res.json(dateObj(new Date()));
 });
 
 // listen for requests :)
